7c34f6f10ba4f85a8ff02b2c8269c1a8996ce828
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-02 13:45:12 +0800

    Finish implementation of sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    else if (typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR";
+    }
+
+    else if (start != parseInt(start) || end != parseInt(end)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    else if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

acc8945959b064aa93ec9b4124c6896799941247
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-02 16:22:09 +0800

    Finish implementation of calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let result = num1 + num2;
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let result = num1 - num2;
+
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let result = 0;
+
+  for (let num of nums) {
+    result += num;
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let result = 1;
 
+  for (let num of nums) {
+    result *= num;
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let result = num ** exponent;
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	// use recursion
+  if (num === 0) {
+    return 1;
+  }
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line

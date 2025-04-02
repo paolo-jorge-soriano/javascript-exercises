2f7df5baacc23968f687ec7168b9210840ed860e
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-02 17:07:58 +0800

    Finish implementation of palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const nonAlphanumericRegex = /[^a-zA-Z0-9]/g;
 
+    text = text.toLowerCase().replace(nonAlphanumericRegex, "");
+    let reversedText = text.split("").reverse().join("");
+
+    if (text === reversedText) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line

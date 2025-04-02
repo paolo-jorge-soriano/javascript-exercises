1ce91ebc52ba7dad108607329777bc888544d790
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-02 15:37:06 +0800

    Finish implementation of tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let result = (temperature - 32) * 5/9;
+
+  if (result !== parseInt(result)) {
+    result = result.toFixed(1);
+  }
+
+  return parseFloat(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let result = (temperature * 9/5) + 32;
+
+  if (result !== parseInt(result)) {
+    result = result.toFixed(1);
+  }
+
+  return parseFloat(result);
 };
 
 // Do not edit below this line

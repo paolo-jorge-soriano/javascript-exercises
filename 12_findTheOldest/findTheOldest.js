403b4c4fae8fc8c6fa7fdeea76e24f82f9ba65f0
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-02 18:59:30 +0800

    Finish implementation of findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+    const currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
 
+    let oldestAge = 0;
+    let personOldest = [];
+
+    for (let person of persons) {
+        if (person.yearOfDeath !== undefined) {
+            if (person.yearOfDeath - person.yearOfBirth > oldestAge) {
+                oldestAge = person.yearOfDeath - person.yearOfBirth;
+                personOldest = person;
+            }
+        }
+
+        else {
+            if (currentYear - person.yearOfBirth > oldestAge) {
+                oldestAge = currentYear - person.yearOfBirth;
+                personOldest = person;
+            }
+        }
+    }
+
+    return personOldest;
 };
 
 // Do not edit below this line
